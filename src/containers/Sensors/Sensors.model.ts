07ab02a369b377311dcbafe93ee2960bce9d2b0e
BePandaMyFriend <al3ix.vidal@gmail.com>
2021-06-11 17:14:15 +0200

    feat: first aprox get sensorS

---
+++ src/containers/Sensors/Sensors.model.ts
@@ -0,0 +1,8 @@
+export interface SensorResponse {
+    id: number;
+    description: string;
+    samplingPeriod: number;
+    isActive: boolean;
+}
+
+export interface ISensors {}
--- src/containers/Sensors/Sensors.tsx
+++ src/containers/Sensors/Sensors.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 
 export const Sensors = () => {
+    const [sensors, setSensors] = useState<any>();
+
+    const getSensors = async () => {
+        const getToken = localStorage.getItem('data');
+        console.log('gettoken', getToken);
+        const headers = {
+            Authorization: `Bearer ${getToken}`
+        };
+        await axios.get(`http://127.0.0.1:8000/api/v1/sensors/`, { headers: headers })
+        .then(sensors => setSensors(sensors))
+        .catch(error => console.log('error', error))
+    }
+    getSensors();
+    useEffect( () => {}, [])
     return (
-        <div>hello Sensors</div>
+        <div>{JSON.stringify(sensors)}</div>
     )
 }
